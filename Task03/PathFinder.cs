36fc5f7e7bbac5c7a5df610bb984e0b58b5637c7
Maksim Shchekin <m.schekin@maguros.ru>
2021-11-27 23:27:35 +0300

    Task03

---
+++ Task03/PathFinder.cs
@@ -0,0 +1,18 @@
+﻿namespace Task03
+{
+    class PathFinder
+    {
+        ILogWriter _logger;
+
+        public PathFinder(ILogWriter logger)
+        {
+            _logger = logger;
+        }
+
+        public void Find(string target)
+        {
+            var msg = $"ищем {target}";
+            _logger?.WriteError(msg);
+        }
+    }
+}
---
+++ Task03/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pathFinder1 = new PathFinder(new ConsoleLogWritter());
+            pathFinder1.Find("только в консоль");
+
+            var pathFinder2 = new PathFinder(new SecureLogWritter(new ConsoleLogWritter()));
+            pathFinder2.Find("только в консоль только по пятницам"); 
+            
+            var pathFinder3 = new PathFinder(new FileLogWritter());
+            pathFinder3.Find("только в файл");
+
+            var pathFinder4 = new PathFinder(new SecureLogWritter(new FileLogWritter()));
+            pathFinder4.Find("только в файл только по пятницам");
+
+            var pathFinder5 = new PathFinder(MultiLogWriter.Create(new ConsoleLogWritter(), new SecureLogWritter(new FileLogWritter())));
+            pathFinder5.Find("в консоль и в файл по пятницам");
+        }
+    }
+}
