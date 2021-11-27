f28e2e21aafdcb62014e542925e84f17c46f8698
Maksim Shchekin <m.schekin@maguros.ru>
2021-11-27 19:00:46 +0300

    Task01 Solution01

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -6,7 +6,29 @@ namespace Task01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // создаем игрока
+            var player = new Player(health: 10);
+            player.OnDamaged += PlayerDamaged;
+            player.OnDie += PlayerDied;
+
+            // создаем бота с оружием
+            var weapon = new Weapon(damage: 6, bullets: 10);
+            var bot = new Bot(weapon);
+
+            bot.OnSeePlayer(player);
+
+            Console.ReadKey();
         }
+
+        private static void PlayerDamaged()
+        {
+            Console.WriteLine("Игрок ранен");
+        }
+
+        private static void PlayerDied()
+        {
+            Console.WriteLine("Игрок убит");
+        }
+
     }
 }
--- Task01/Weapon.cs
+++ Task01/Weapon.cs
@@ -1,29 +1,89 @@
-﻿namespace Task01
-{
-    class Weapon
-    {
-        public int Damage;
-        public int Bullets;
+﻿using System;
 
-        public void Fire(Player player)
+namespace Task01
+{
+    class Player
+    {
+        private int _health;
+
+        public Action OnDamaged;
+        public Action OnDie;
+
+        public bool IsAlive => _health > 0;
+
+        public Player(int health)
         {
-            player.Health -= Damage;
-            Bullets -= 1;
+            if (health > 0)
+                _health = health;
+            else
+                throw new ArgumentOutOfRangeException(nameof(health));
+        }
+
+        public void TakeDamage(int damage)
+        {
+            if (damage > 0)
+                _health -= damage;
+            else
+                throw new ArgumentOutOfRangeException(nameof(damage));
+
+            if (_health < 0)
+                _health = 0;
+
+            if (_health > 0)
+                OnDamaged?.Invoke();
+            else
+                OnDie?.Invoke();
         }
     }
 
-    class Player
+    class Weapon
     {
-        public int Health;
+        private int _damage;
+        private int _bullets;
+
+        public Weapon(int damage, int bullets)
+        {
+            if (damage > 0)
+                _damage = damage;
+            else
+                throw new ArgumentOutOfRangeException(nameof(damage));
+
+            if (bullets > 0)
+                _bullets = bullets;
+            else
+                throw new ArgumentOutOfRangeException(nameof(bullets));
+        }
+
+        public bool HaveBullets => _bullets > 0;
+
+        public void Fire(Player player)
+        {
+            if (HaveBullets)
+            {
+                _bullets -= 1;
+                player.TakeDamage(_damage);
+            }
+            else
+            {
+                throw new Exception("кончились патроны");
+            }
+        }
     }
 
     class Bot
     {
-        public Weapon Weapon;
+        public Weapon _weapon;
+
+        public Bot(Weapon weapon)
+        {
+            _weapon = weapon;
+        }
 
         public void OnSeePlayer(Player player)
         {
-            Weapon.Fire(player);
+            while (player.IsAlive
+                && _weapon.HaveBullets)
+                _weapon.Fire(player);
         }
     }
 }
