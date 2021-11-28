040df3d4aed6fc53d4b1ea9ca7f3d9e07abfec40
Maksim Shchekin <m.schekin@maguros.ru>
2021-11-28 20:06:08 +0300

    Task04

---
+++ Task04/PaymentSystem.cs
@@ -0,0 +1,92 @@
+﻿namespace Task04
+{
+    public interface IPaymentSystem
+    {
+        public string GetPayingLink();
+    }
+
+    public abstract class PaymentSystemBase : IPaymentSystem
+    {
+        private string _baseLink;
+        private IHash _hash;
+        protected string Hash => _hash.Create(GetHashInput());
+        protected Order _order;
+
+        protected PaymentSystemBase(string baseLink, Order order, IHash hash)
+        {
+            _baseLink = baseLink;
+            _order = order;
+            _hash = hash;
+        }
+
+        public string GetPayingLink()
+        {
+            var parameters = GetLinkParameters();
+            var fullLink = $"{_baseLink}?{parameters}";
+            return fullLink;
+        }
+
+        protected abstract string GetLinkParameters();
+
+        protected abstract string GetHashInput();
+    }
+
+    /// <summary>
+    /// pay.system1.ru/order?amount=12000RUB&hash={MD5 хеш ID заказа}
+    /// </summary>
+    public class PaymentSystem1 : PaymentSystemBase
+    {
+        public PaymentSystem1(Order order) : base(baseLink: "pay.system1.ru/order", order, new HashMD5()) { }
+
+        protected override string GetLinkParameters()
+        {
+            return $"amount={_order.Amount}RUB&hash={Hash}";
+        }
+
+        protected override string GetHashInput()
+        {
+            return _order.Id.ToString();
+        }
+    }
+
+    /// <summary>
+    /// order.system2.ru/pay?hash={MD5 хеш ID заказа + сумма заказа}
+    /// </summary>
+    public class PaymentSystem2 : PaymentSystemBase
+    {
+        public PaymentSystem2(Order order) : base(baseLink: "order.system2.ru/pay", order, new HashMD5()) { }
+
+        protected override string GetLinkParameters()
+        {
+            return $"hash={Hash}";
+        }
+
+        protected override string GetHashInput()
+        {
+            return (_order.Id + _order.Amount).ToString();
+        }
+    }
+
+    /// <summary>
+    /// system3.com/pay?amount=12000&curency=RUB&hash={SHA-1 хеш сумма заказа + ID заказа + секретный ключ от системы}
+    /// </summary>
+    public class PaymentSystem3 : PaymentSystemBase
+    {
+        string _secretKey;
+
+        public PaymentSystem3(Order order, string secretKey) : base(baseLink: "system3.com/pay", order, new HashSHA1()) 
+        {
+            _secretKey = secretKey;
+        }
+
+        protected override string GetLinkParameters()
+        {
+            return $"amount={_order.Amount}&currency=RUB&hash={Hash}";
+        }
+
+        protected override string GetHashInput()
+        {
+            return _order.Id.ToString() + _order.Amount.ToString() + _secretKey;
+        }
+    }
+}
---
+++ Task04/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Task04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var order = new Order(id: 12345678, amount: 12000);
+
+            //Выведите платёжные ссылки для трёх разных систем платежа: 
+            //pay.system1.ru/order?amount=12000RUB&hash={MD5 хеш ID заказа}
+            var ps1 = new PaymentSystem1(order);
+            Console.WriteLine(ps1.GetPayingLink());
+
+            Console.WriteLine();
+
+            //order.system2.ru/pay?hash={MD5 хеш ID заказа + сумма заказа}
+            var ps2 = new PaymentSystem2(order);
+            Console.WriteLine(ps2.GetPayingLink());
+
+            Console.WriteLine();
+
+            //system3.com/pay?amount=12000&curency=RUB&hash={SHA-1 хеш сумма заказа + ID заказа + секретный ключ от системы}
+            var secretKey = "114E54176B9649339F38978B9A682FB2";
+            var ps3 = new PaymentSystem3(order, secretKey);
+            Console.WriteLine(ps3.GetPayingLink());
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Order
+    {
+        public readonly int Id;
+        public readonly int Amount;
+
+        public Order(int id, int amount) => (Id, Amount) = (id, amount);
+    }
+}
